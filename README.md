2c5866d80b66b20feb9b393eff430c191c979b02
Sho DOUHASHI <douhashi@gmail.com>
2017-02-24 16:59:04 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Redmine System Info Plugin
---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_system_info do
+  name 'Redmine System Info plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
